683bd282ae8279b43130ca1346df990ff345ea19
Juraj Mičko <micko.juraj64@gmail.com>
2015-11-16 14:43:19 +0100

    hardware: Created button constants

---
+++ src/main/java/net/talentum/fbp/hardware/Button.java
@@ -0,0 +1,14 @@
+package net.talentum.fbp.hardware;
+
+/**
+ * Enumerates all three buttons on FBP.
+ * 
+ * @author JJurM
+ */
+public enum Button {
+
+	LEFT,
+	OK,
+	RIGHT;
+	
+}
---
+++ src/main/java/net/talentum/fbp/hardware/ButtonState.java
@@ -0,0 +1,13 @@
+package net.talentum.fbp.hardware;
+
+/**
+ * Enumerates two possible states of button.
+ * 
+ * @author JJurM
+ */
+public enum ButtonState {
+
+	PRESSED,
+	RELEASED;
+	
+}
