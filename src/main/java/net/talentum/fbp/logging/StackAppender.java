e3444e62a3f70e64fa26970bafd1453a36db26d6
Juraj Mičko <micko.juraj64@gmail.com>
2015-11-29 21:30:02 +0100

    log4j2: Added factory method for StackAppender

--- src/main/java/net/talentum/fbp/logging/StackAppender.java
+++ src/main/java/net/talentum/fbp/logging/StackAppender.java
@@ -1,12 +1,21 @@
 package net.talentum.fbp.logging;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.util.Booleans;
 
 /**
  * This {@link Appender} stacks logged events until
@@ -18,6 +27,7 @@ import org.apache.logging.log4j.core.config.plugins.Plugin;
  */
 @Plugin(name = "Stack", category = "Core", elementType = "appender", printObject = true)
 public class StackAppender extends AbstractAppender {
+	private static final Logger LOG = LogManager.getLogger();
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -25,8 +35,30 @@ public class StackAppender extends AbstractAppender {
 	 * 
 	 * @param name
 	 */
-	protected StackAppender(String name) {
-		super(name, null, null, true);
+	protected StackAppender(String name, boolean ignoreExceptions) {
+		super(name, null, null, ignoreExceptions);
+	}
+
+	/**
+	 * Plugin factory method provided for Log4j2.
+	 * 
+	 * @param layout
+	 * @param filter
+	 * @param name
+	 * @param ignore
+	 * @return created {@link StackAppender} or {@code null} on failure.
+	 */
+	@PluginFactory
+	public static StackAppender createAppender(@PluginElement("Layout") Layout<? extends Serializable> layout,
+			@PluginElement("Filters") final Filter filter, @PluginAttribute("name") final String name,
+			@PluginAttribute(value = "ignoreExceptions", defaultBoolean = true) final String ignore) {
+
+		if (name == null) {
+			LOG.error("log4j2: No name provided for StackAppender");
+			return null;
+		}
+		final boolean ignoreExceptions = Booleans.parseBoolean(ignore, true);
+		return new StackAppender(name, ignoreExceptions);
 	}
 
 	protected List<LogEvent> logEvents = new ArrayList<LogEvent>();
@@ -45,7 +77,8 @@ public class StackAppender extends AbstractAppender {
 	 * Sets current appender to the new one. Stacked events and every new events
 	 * will be logged directly to the given appender.
 	 * 
-	 * @param appender new {@link Appender}
+	 * @param appender
+	 *            new {@link Appender}
 	 */
 	public void setAppender(Appender appender) {
 		this.appender = appender;
