8222c88baee280125159bb7c26d8de372acdcfda
Juraj Mičko <micko.juraj64@gmail.com>
2015-12-12 20:33:01 +0100

    hardware: Corrected pin definitions
    Pi4j library uses abstract WiringPi pin number scheme instead of standard GPIO

--- src/main/java/net/talentum/fbp/hardware/Pins.java
+++ src/main/java/net/talentum/fbp/hardware/Pins.java
@@ -10,26 +10,26 @@ import com.pi4j.io.gpio.RaspiPin;
  */
 public class Pins {
 
-	public static Pin PIN_LED_ON = RaspiPin.GPIO_14;
-	public static Pin PIN_LED_ACT = RaspiPin.GPIO_17;
-	public static Pin PIN_LED_ERR = RaspiPin.GPIO_04;
+	public static Pin PIN_LED_ON = RaspiPin.GPIO_15;
+	public static Pin PIN_LED_ACT = RaspiPin.GPIO_00;
+	public static Pin PIN_LED_ERR = RaspiPin.GPIO_07;
 
-	public static Pin PIN_BTN_L = RaspiPin.GPIO_27;
-	public static Pin PIN_BTN_OK = RaspiPin.GPIO_22;
-	public static Pin PIN_BTN_R = RaspiPin.GPIO_10;
+	public static Pin PIN_BTN_L = RaspiPin.GPIO_02;
+	public static Pin PIN_BTN_OK = RaspiPin.GPIO_03;
+	public static Pin PIN_BTN_R = RaspiPin.GPIO_12;
 
-	public static Pin PIN_HALL = RaspiPin.GPIO_26;
+	public static Pin PIN_HALL = RaspiPin.GPIO_25;
 
-	public static Pin PIN_PIEZO = RaspiPin.GPIO_21;
+	public static Pin PIN_PIEZO = RaspiPin.GPIO_29;
 
-	public static Pin PIN_LCD_BCKLGHT = RaspiPin.GPIO_20;
-	public static Pin PIN_LCD_RS = RaspiPin.GPIO_09;
-	public static Pin PIN_LCD_E = RaspiPin.GPIO_11;
+	public static Pin PIN_LCD_BCKLGHT = RaspiPin.GPIO_28;
+	public static Pin PIN_LCD_RS = RaspiPin.GPIO_13;
+	public static Pin PIN_LCD_E = RaspiPin.GPIO_14;
 	public static Pin[] PINS_LCD_DB = {
-			RaspiPin.GPIO_05, 
-			RaspiPin.GPIO_06, 
-			RaspiPin.GPIO_13, 
-			RaspiPin.GPIO_19
+			RaspiPin.GPIO_21,
+			RaspiPin.GPIO_22,
+			RaspiPin.GPIO_13,
+			RaspiPin.GPIO_24
 	};
 
 }
