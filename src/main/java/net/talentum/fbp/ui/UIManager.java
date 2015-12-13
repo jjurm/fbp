13abd213ff687206a6244187c6148f841d9d3a9f
Juraj Mičko <micko.juraj64@gmail.com>
2015-12-13 12:12:08 +0100

    ui: Defined elementary menu hierarchy (UIManager)

--- src/main/java/net/talentum/fbp/ui/UIManager.java
+++ src/main/java/net/talentum/fbp/ui/UIManager.java
@@ -6,10 +6,17 @@ import org.apache.logging.log4j.Logger;
 import net.talentum.fbp.context.Context;
 import net.talentum.fbp.context.ContextHolder;
 import net.talentum.fbp.context.HomeScreen;
+import net.talentum.fbp.context.LogBrowser;
+import net.talentum.fbp.context.StaticInfoContext;
+import net.talentum.fbp.context.menu.BackMenuItem;
+import net.talentum.fbp.context.menu.MainMenu;
+import net.talentum.fbp.context.menu.Menu;
+import net.talentum.fbp.context.menu.ShutdownMenuItem;
 import net.talentum.fbp.display.RedrawRequestHandler;
 import net.talentum.fbp.hardware.button.ButtonEvent;
 import net.talentum.fbp.hardware.button.ButtonEventHandler;
 import net.talentum.fbp.hardware.drivers.DisplayDriver;
+import net.talentum.fbp.system.Config;
 
 /**
  * Class that is responsible for rendering content on display. It does not
@@ -53,7 +60,25 @@ public class UIManager implements ButtonEventHandler, RedrawRequestHandler, Cont
 	 * underlying {@link Context}s.
 	 */
 	protected void constructMenuHierarchy() {
+		LogBrowser logBrowser = new LogBrowser(this);
+
+		// @formatter:off
 		homeScreen = new HomeScreen(this);
+		homeScreen.setMainMenu(
+				new MainMenu(homeScreen, this)
+					.addMenuItem(new Menu("Logs", this)
+							.addMenuItem(logBrowser.new LogLevelSelection())
+							.addMenuItem(logBrowser)
+							.addMenuItem(new BackMenuItem())
+					)
+					.addMenuItem(new Menu("Settings", this)
+							.addMenuItem(new BackMenuItem())
+					)
+					.addMenuItem(new StaticInfoContext("About FBP", new AutoWrapStringContent("FBP project is developed with care and enthusiasm.", Config.getDisplayColumns(), 1), this))
+					.addMenuItem(new ShutdownMenuItem())
+					.addMenuItem(new BackMenuItem())
+		);
+		// @formatter:on
 	}
 
 	@Override
